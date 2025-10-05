a768344bb5ce6cdb7847770fe515b5d44106659e
Aniket2608 <aniketguleria392002@gmail.com>
2025-10-05 19:29:05 +0530

    Create Student.java

---
+++ Java-exp6/Student.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private String department;
+    private double marks;
+
+    public Student(int studentID, String name, String department, double marks) {
+        this.studentID = studentID;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public Student(String name, String department, double marks) {
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getStudentID() { return studentID; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getMarks() { return marks; }
+
+    @Override
+    public String toString() {
+        return String.format("%d | %s | %s | %.2f", studentID, name, department, marks);
+    }
+}
